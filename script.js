754d11eb8404e2e898dfbcc70959427c62306acd
yunusemrekarkale <mevlutkarkale52@gmail.com>
2025-03-13 11:10:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const users = [
-    { username: 'YunusEmreKARKALE', password: 'YunusEmre42.' },
-    { username: 'KadirDİNÇ', password: 'Kadir42.' },
-    { username: 'TarıkSARICI', password: 'Tarık42.'},
-    { username: 'NurullahÇELİK', password: 'Nurullah42.'},
-    // Diğer kullanıcılar...
-];
-
 function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const user = users.find(user => user.username === username && user.password === password);
-
-    if (user) {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if (username === 'h126725', 'H126725' && password === 'YunusEmreKARKALE') {
         document.getElementById('login').style.display = 'none';
         document.getElementById('main-content').style.display = 'block';
     } else {
         document.getElementById('error-message').style.display = 'block';
     }
 }
+
+function showContent(id) {
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(function(content) {
+        content.style.display = 'none';
+    });
+
+    var selectedContent = document.getElementById(id);
+    if (selectedContent) {
+        selectedContent.style.display = 'block';
+    }
+    
+        return
+}
+
